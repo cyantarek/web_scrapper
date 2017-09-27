341d3901716fbe5cc478755cd8ccea63e0e922d2
Cyan Tarek <cyantarek.cg@gmail.com>
2017-09-27 22:40:28 +0600

    Project Created!

--- scrapper.py
+++ scrapper.py
@@ -1,1 +1,8 @@
-print("Hello")
+from bs4 import BeautifulSoup, url
+
+page = url("http://pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/")
+soup = BeautifulSoup(page,'lxml')
+all_data = soup.find_all("div", {"class":"propertyRow"})
+price = all_data[0].find("h4", {"class":"propertyPrice"})
+print(all_data)
+
