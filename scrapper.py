1c75a6717f6c4b21402e0181869de718c2deac3a
Cyan Tarek <cyantarek.cg@gmail.com>
2017-09-30 00:01:47 +0600

    Scrapper Newegg

--- scrapper.py
+++ scrapper.py
@@ -1,8 +1,5 @@
 from bs4 import BeautifulSoup, url
 
-page = url("http://pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/")
+page = url("https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38")
 soup = BeautifulSoup(page,'lxml')
-all_data = soup.find_all("div", {"class":"propertyRow"})
-price = all_data[0].find("h4", {"class":"propertyPrice"})
-print(all_data)
 
