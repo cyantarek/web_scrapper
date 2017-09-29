c239bb8629bf0758dcb0ecd18d5cbedee2b1f3cb
Cyan Tarek <cyantarek.cg@gmail.com>
2017-09-30 03:22:25 +0600

    Scrapper Newegg - Almost complete!

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup, url
-import csv
 import pandas
 import matplotlib.pyplot as plt
+import pymysql
+from sqlalchemy import create_engine
+import _mysql
 
 page = url("https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38")
 soup = BeautifulSoup(page, 'lxml')
@@ -21,6 +23,8 @@ card_details = pandas.DataFrame({
     "Shipping": shipping
 })
 card_details.to_csv("output.csv")
+conn = pymysql.connect(host="127.0.0.1", port=80, user="root", password="", db="test", charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+engine = create_engine('mysql+pymysql://[root]:[]@[127.0.0.1]:[80]/[test]', echo=False)
+card_details.to_sql(name="scrap_tb", con=engine, schema=None, if_exists='fail', index=True, index_label=None, chunksize=None, dtype=None)
 
-card_details.plot(kind="line")
-plt.show()
+
