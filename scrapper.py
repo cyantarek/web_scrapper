ca71e3bca5fabba072f95407682c68fc7061e8c2
Cyan Tarek <cyantarek.cg@gmail.com>
2017-09-30 01:07:11 +0600

    Scrapper Newegg - Scrapped!

--- scrapper.py
+++ scrapper.py
@@ -3,3 +3,12 @@ from bs4 import BeautifulSoup, url
 page = url("https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38")
 soup = BeautifulSoup(page,'lxml')
 
+cards = soup.find_all("div", {"class":"item-container"})
+
+for card in cards:
+    card_brand = card.div.div.a.img["title"]
+    card_name = card.find_all("a", {"class":"item-title"})[0].text
+    shipping = card.find_all("li", {"class":"price-ship"})[0].text.replace(" ", "").strip()
+
+    print(shipping)
+
